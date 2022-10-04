050c2de46eda6e021dc27da74cc93a0b21b1a89a
Wesley Lewis <lewis.wes.23@gmail.com>
2022-10-04 22:56:35 +0530

    Added Login.jsx file

--- frontend/src/Components/pages/Home/Nav.jsx
+++ frontend/src/Components/pages/Home/Nav.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React from 'react'
 var mySidebar = document.getElementById("mySidebar");
-import laptopNavbar from './laptopNavbar';
-import mobileNavbar from './mobileNavbar';
+
 
 function w3_open() {
   if (mySidebar.style.display === 'block') {
@@ -20,22 +18,35 @@ function w3_close() {
 
 
 const Nav = () => {
-  const [width, setWidth] = useState(window.innerWidth)
-  console.log(width)
+  return (
+    <div>
+      <div className="w3-top">
+      <div className="w3-bar w3-white w3-card" id="myNavbar">
+        <a href="#Home" className="w3-bar-item w3-button w3-wide"><img src="Logo.png" alt="" style={{width: "30px"}} />SHAW'rganizer</a>
+        <div className="w3-right w3-hide-small">
+          <a href="#We Serve" className="w3-bar-item w3-button">HOME</a>
+          <a href="#Amenities" className="w3-bar-item w3-button">EVENTS</a>
+          <a href="#About Us" className="w3-bar-item w3-button">NEWS</a>
+          <a href="#Gallery" className="w3-bar-item w3-button">SUBMIT AN EVENT</a>
+          <a href="#Contact Us" className="w3-bar-item w3-button">HOW IT WORK?</a>
+        </div>
+        <a href="." className="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onClick={() => w3_open()}>
+        <i className="fa fa-bars"></i>
+      </a>
+      </div>
+      
+    </div>
 
-  useEffect(() => {
-    function handleResize() {
-      setWidth(window.innerWidth)
-    }
-
-    window.addEventListener('resize', handleResize)
-  }
+  <div className="w3-sidebar w3-black w3-bar-block w3-collapse w3-card w3-animate-right" style={{width:"200px", right:0}} id="mySidebar">
+    <button className="w3-bar-item w3-button w3-large w3-hide-large" onclick="w3_close()">Close &times;</button>
+    <a href="#We Serve" className="w3-bar-item w3-button">HOME</a>
+          <a href="#Amenities" className="w3-bar-item w3-button">EVENTS</a>
+          <a href="#About Us" className="w3-bar-item w3-button">NEWS</a>
+          <a href="#Gallery" className="w3-bar-item w3-button">SUBMIT AN EVENT</a>
+          <a href="#Contact Us" className="w3-bar-item w3-button">HOW IT WORK</a>
+  </div>
+    </div>
   )
-
-  if(width > 500) {
-    return <laptopNavbar />
-  }
-    return <mobileNavbar />
 }
 
 export default Nav
