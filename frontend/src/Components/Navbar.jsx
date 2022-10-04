d523915e0b05ca16e9e7a50cf08cee614e4b545d
Wesley Lewis <lewis.wes.23@gmail.com>
2022-10-04 19:56:07 +0530

    Converted Basic HTML to React

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import {Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/contact">Contact</Link></li>       
+            </ul>
+        </nav>
+    </header>
   )
 }
 
---
+++ frontend/src/Components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+        <div>
+            <button><a href="login.html">Login</a></button>            
+        </div>
+    </main>
+  )
+}
+
+export default Home
