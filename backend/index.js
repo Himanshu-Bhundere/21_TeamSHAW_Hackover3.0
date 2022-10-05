bc37bd096a301dc046da79e6cd6b29a7b310cb1d
Wesley Lewis <lewis.wes.23@gmail.com>
2022-10-05 10:40:29 +0530

    index and main.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname));
